9b8d47d0652a4cf9b2d6a95ffd86e59ac86586df
David <davidphang431@gmail.com>
2021-08-16 13:17:21 +0700

    Initial comit

---
+++ models/index.js
@@ -0,0 +1,13 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+}); // Config environment
+const mongoose = require("mongoose");
+const uri = mongoose
+  .connect(process.env.MONGO_URI, {
+    useUnifiedTopology: true, // must be added
+    useNewUrlParser: true, // must be added
+    useCreateIndex: true, // use to enable unique data type
+    useFindAndModify: false, // use findoneandupdate instead of findandmodify
+  })
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
