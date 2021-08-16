43bd77a06a19bf3a14b1ae825f96929243a061ed
David <davidphang431@gmail.com>
2021-08-16 15:09:25 +0700

    Create package models

---
+++ config/services.js
@@ -0,0 +1,3 @@
+const venueServices = ["electricity", "security", "cleaning service"];
+
+const organizerServices = ["planning", "organizer"];
---
+++ models/packages.js
@@ -0,0 +1,70 @@
+const mongoose = require("mongoose");
+const mongooseDelete = require("mongoose-delete");
+
+const packageSchema = new mongoose.Schema(
+  {
+    package_vendor_id: {
+      type: mongoose.Types.ObjectId,
+      ref: "Vendor",
+      required: true,
+    },
+    package_type: {
+      type: String,
+      required: true,
+    },
+    package_name: {
+      type: String,
+    },
+    package_location: {
+      type: String,
+    },
+    package_price: {
+      type: String,
+    },
+    package_details: {
+      type: String,
+    },
+    package_services: [
+      {
+        type: String,
+      },
+    ],
+    package_album: [
+      {
+        type: String,
+      },
+    ],
+    package_details: {
+      type: String,
+    },
+  },
+  {
+    // Enables timestamps
+    timestamps: {
+      createdAt: "created_at",
+      updatedAt: "updated_at",
+    },
+    toObject: { getters: true },
+    toJSON: {
+      getters: true,
+      versionKey: false,
+      transform: function (doc, ret) {
+        ret.package_id = ret._id;
+        delete ret.id;
+        delete ret.deleted;
+      },
+    },
+  }
+);
+
+// movieSchema.virtual("requests", {
+//   ref: "Request",
+//   localField: "_id",
+//   foreignField: "movie_id",
+//   justOne: false,
+// });
+
+// Enable soft delete, it will make delete column automaticly
+movieSchema.plugin(mongooseDelete, { overrideMethods: "all" });
+
+module.exports = mongoose.model("Movie", movieSchema); // Export transaction models
